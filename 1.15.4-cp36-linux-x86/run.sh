b17d6638ec154b852516b01d9539504c25e7f1e8
Chris Partridge <chris@partridge.tech>
2020-12-25 23:48:36 -0500

    Update run.sh

--- 1.15.4-cp36-linux-x86/run.sh
+++ 1.15.4-cp36-linux-x86/run.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -e
 
-echo "[GPT2S/TFB] Configuring build for tensorflow 1.15.4 on py3.6 with defaults for your CPU architecture ..."
-sleep 5
+echo "[TDBE] Configuring build for tensorflow 1.15.4 on py3.6 with defaults for your CPU architecture ..."
 cd /build/tensorflow
 python3 configure.py
 
-echo "[GPT2S/TFB] Building tensorflow with bazel ..."
-echo "[GPT2S/TFB] Please put a Gatorade on your nightstand, chug [safe_quantity] beers, and check in the morning"
-sleep 5
+echo "[TDBE] Building tensorflow with bazel ..."
+echo "[TDBE] Please put a Gatorade on your nightstand, chug [safe_quantity] beers, and check in the morning"
 bazel build --config=opt //tensorflow/tools/pip_package:build_pip_package
 
-echo "[GPT2S/TFB] Building pip package for tensorflow ..."
-sleep 5
+echo "[TDBE] Building pip package for tensorflow ..."
 ./bazel-bin/tensorflow/tools/pip_package/build_pip_package /mnt
 
-echo "[GPT2S/TFB] Exiting normally - please install tensorflow from the wheel file in the directory you specified"
+echo "[TDBE] Exiting normally - please install tensorflow from the wheel file in the directory you specified"
